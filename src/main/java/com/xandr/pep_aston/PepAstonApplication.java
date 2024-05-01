24f1210d1b1bb7a7095beb23900b5292ecbea6ef
Pep_$xx <139212629+pepsxx@users.noreply.github.com>
2024-05-01 16:45:12 +0300

    Initial commit

---
+++ src/main/java/com/xandr/pep_aston/PepAstonApplication.java
@@ -0,0 +1,13 @@
+package com.xandr.pep_aston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepAstonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PepAstonApplication.class, args);
+    }
+
+}
